f05e127d1338164ce71e7c5643a9e98533fd4656
Jerome Manarang <jemanara@uci.edu>
2024-03-08 17:53:53 -0800

    Added functionality that allows targetpos to be provided for front and back functions

--- lib/Arm/Arm.cpp
+++ lib/Arm/Arm.cpp
@@ -14,25 +14,29 @@ void Arm::init(int pinOne, int pinTwo, int pinThree)
 }
 
 
-void Arm::moveForward(int currentpos)
+void Arm::moveForward(int currentpos, int targetpos)
 {
-    for (pos = 0; pos <= 180; pos += 25) {
+    for (pos = currentpos; targetpos <= 180; pos += 25) {
     // tell servo to go to position in variable 'pos'
     servo_9.write(pos);
     servo_10.write(pos);
     // wait 15 ms for servo to reach the position
     delay(15); // Wait for 15 millisecond(s)
+
+    // TODO: change currentpos to target pos
 }
  }
 
 
 
-void Arm::moveBackward(int currentpos);
+void Arm::moveBackward(int currentpos, int targetpos);
 {
-    for (pos = 0; pos <= 180; pos -= 25)
+    for (pos = currentpos; pos <= targetpos; pos -= 25)
     // tell servo to go to position in variable 'pos'
     servo_9.write(pos);
     servo_10.write(pos);
     // wait 15 ms for servo to reach the position
     delay(15); // Wait for 15 millisecond(s)
+
+    // TODO: change currentpos to target pos
 }
