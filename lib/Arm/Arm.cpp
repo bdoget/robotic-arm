7e960343c4ae6235991db14adf3db638412e20b2
Jerome Manarang <jemanara@uci.edu>
2024-03-15 16:59:36 -0700

    cleaned up code

--- lib/Arm/Arm.cpp
+++ lib/Arm/Arm.cpp
@@ -3,12 +3,9 @@
 
 // --------
 
-void Arm::init(int pinOne, int pinTwo, int pinThree)
+void Arm::init(int pinOne)
 {
     pinOne = pinOne;
-    pinTwo = pinTwo;
-    pinThree = pinThree;
-
 
 }
 
@@ -16,33 +13,13 @@ void Arm::init(int pinOne, int pinTwo, int pinThree)
 // Below, we need current pos of each servo stored elsewhere, then passed
 // into functions below, the target pos will be returned. 
 
-float Arm::servoMoveForward(float currentpos, float targetpos)
+float Arm::servoMoveForward(float targetpos)
 {
-    for (pos = currentpos; pos <= targetpos; pos += 25) {
-    // tell servo to go to position in variable 'pos'
-    servo_9.write(pos);
-    servo_10.write(pos);
+    servoBase.write(targetpos)
     // wait 15 ms for servo to reach the position
     delay(15); // Wait for 15 millisecond(s)
-    return target pos
-
-    // TODO: change currentpos to target pos
-}
- }
-
-
-
-float Arm::servoMoveBackward(float currentpos, float targetpos);
-{
-    for (pos = currentpos; pos >= targetpos; pos -= 25)
-    // tell servo to go to position in variable 'pos'
-    servo_9.write(pos);
-    servo_10.write(pos);
-    // wait 15 ms for servo to reach the position
-    delay(15); // Wait for 15 millisecond(s)
-    
     return targetpos
 }
+ 
 
 
-///////// TEST  ///////////
--- lib/Arm/Arm.h
+++ lib/Arm/Arm.h
@@ -1,9 +1,13 @@
 #ifndef Arm_H
 #define Arm_H
+#include <Servo.h>
 
 class Arm {
     public:
         void init(int pinOne, int pinTwo, int pinThree);
+        Servo1.attach(pinOne);
+        Servo2.attach(pinTwo);
+        Servo3.attach(pinThree);
         void servoMoveForward(int currentpos, int targetpos);
         void servoMoveBackward(int currentpos, int targetpos);
     
@@ -12,4 +16,11 @@ class Arm {
         int pinOne;
         int pinTwo;
         int pinThree;
+        Servo Servo1;
+        Servo Servo2;
+        Servo Servo3;
+    
 };
+
+
+
