25c9064c278688da6717ae464b351a4bd580cdc3
bdoget <bdogetlauncher@gmail.com>
2024-04-18 14:27:45 -0700

    Added rest position for arm

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,12 @@ Servo servo1;
 Servo servo2;
 Servo servo3;
 Servo servo4;
-Servo servo5;
 
 void setup() {
   servo1.attach(A5); // claw
   servo2.attach(A1); // shoulder
   servo3.attach(A3); // wrist
   servo4.attach(A2); // elbow
-  servo5.attach(12);
 
   Serial.begin(9600);
   Serial.print("Done Setup");
@@ -23,17 +21,17 @@ void writeAll(int num){
   servo2.write(num);
   servo3.write(num);
   servo4.write(num);
-  servo5.write(num);
   Serial.print("Go to num: ");
   Serial.println(num);
   delay(2000);
 }
 
-// void test(int num) {
-//   switch (num) {
-
-//   }
-// }
+void gotoRest() {
+  servo1.write(0);
+  servo2.write(0);
+  servo3.write(90);
+  servo4.write(0);
+}
 
 void loop() {
   // writeAll(45);
