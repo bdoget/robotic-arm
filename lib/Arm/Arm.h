a4d945c4e86d9cd9dc7a22ac97bb05759898084e
Jerome Manarang <jemanara@uci.edu>
2024-04-18 14:39:00 -0700

    4 servo functionality (testing)

--- lib/Arm/Arm.h
+++ lib/Arm/Arm.h
@@ -2,7 +2,14 @@
 #define Arm_H
 #include <Servo.h>
 
+/*
+
+
+*/
+
+
 // NEEDED: A call to the arm class that includes the pins of each servo
+
 //         SERVO 1: elbow
 //         SERVO 2: wrist
 //         SERVO 3: claw
@@ -11,15 +18,13 @@
 
 class Arm {
     public:
-        void init(int pin1, int pin2, int pin3);
-        void moveServo1(int angle);
-        void moveServo2(int angle);
-        void moveServo3(int angle);
-    
+        void init(int pin1, int pin2, int pin3, int pin4);
+        void moveServo(int servoNum, int angle);
+
     private:
-        Servo Servo1;
-        Servo Servo2;
-        Servo Servo3;
+        Servo servos[4];
+        int mins[4];
+        int maxs[4];
     
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -9,31 +9,33 @@ void setup()
 {
   Serial.begin(9600);
   Serial.println("Done with setup");
-
-
-  arm.init(A0,2,3);
-  arm.moveServo1(0);
-
-  // servo1.attach(A0);
-  // servo1.write(0);
-
+  arm.init(A0,A1,A2,A3); // place pins in order of this: elbow, wrist, claw 
   delay(1000);
 }
 
 void loop() {
+  //arm.moveServo(0,0);
+  //delay(1000);
+  //arm.moveServo(0,85);
+  //delay(1000);
+
   for (int i=0; i < 1; i++) {
-    arm.moveServo1(90);
-    // servo1.write(0);
-    delay(1000);
-    arm.moveServo1(180);
 
-    // servo1.write(90);
+    arm.moveServo(0,0);
     delay(1000);
-    arm.moveServo1(0);
-
-    // servo1.write(180);
-
+    arm.moveServo(0,75);
     delay(1000);
+
+    //arm.moveServo(1,90);
+    //delay(1000);
+
+    //arm.moveServo(2,90);
+    //delay(1000);
+    //arm.moveServo(3,0);
+    //delay(1000);
+
+
+    
   }
   Serial.println("looped Done");
   delay(1000);
