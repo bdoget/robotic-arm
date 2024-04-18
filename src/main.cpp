8109743af42bd397de9051fe03e31d271179fe52
bdoget <bdogetlauncher@gmail.com>
2024-04-18 14:15:23 -0700

    Servo control

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,44 @@
 #include "Arduino.h"
+#include "Servo.h"
 
-void setup()
-{
+Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
+Servo servo5;
+
+void setup() {
+  servo1.attach(A5); // claw
+  servo2.attach(A1); // shoulder
+  servo3.attach(A3); // wrist
+  servo4.attach(A2); // elbow
+  servo5.attach(12);
+
   Serial.begin(9600);
-  Serial.println("Done with setup");
+  Serial.print("Done Setup");
 }
 
+void writeAll(int num){
+  servo1.write(num);
+  servo2.write(num);
+  servo3.write(num);
+  servo4.write(num);
+  servo5.write(num);
+  Serial.print("Go to num: ");
+  Serial.println(num);
+  delay(2000);
+}
+
+// void test(int num) {
+//   switch (num) {
+
+//   }
+// }
+
 void loop() {
-  Serial.println("In loop now");
-  delay(1000);
+  // writeAll(45);
+  // writeAll(135);
+  writeAll(0);
+  writeAll(45);
+  writeAll(90);
 }
