e0e64c1e9964840a1be9b7270cdb3cf666afaf77
bdoget <bdogetlauncher@gmail.com>
2024-05-16 22:02:34 -0700

    Add Radio test

--- lib/Tests/test-arm.h
+++ lib/Tests/test-arm.h
@@ -9,21 +9,20 @@ void test_setup()
 {
   Serial.begin(9600);
   Serial.println("Done with setup");
-  arm.init(A0,A1,A2,A3); // place pins in order of this: elbow, wrist, claw 
+  arm.init(A1,A2,A3,A4); // place pins in order of this: shoulder,elbow, wrist, claw 
   delay(1000);
 }
 
 void test_loop() {
 
-  for (int i=0; i < 1; i++) {
-
-    arm.moveServo(0,0);
-    delay(1000);
-    arm.moveServo(0,75);
-    delay(1000);
-    
-  }
-  Serial.println("looped Done");
+  // for (int i=0; i < 4; i++) {}
+  arm.gotoRest();
   delay(1000);
+
+  // arm.moveServo(0 ,0);
+  // delay(1000);
+  // arm.moveServo(0,90);
+  // Serial.println("looped NEW");
+  // delay(1000);
 }
 #endif
