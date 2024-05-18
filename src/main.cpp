2c16f53d0c5fec67b45b29fbef9f9706f00cf245
Arvick <macalaladjustin@gmail.com>
2024-05-17 17:42:46 -0700

    new main

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,13 @@
 #include "test-arm.h"
 
 void setup() {
+  Serial.println("Start with setup");
   test_setup();
+  Serial.println("Setup done");
 }
 
 void loop() {
-  test_loop();
+  //test_loop();
+  Serial.println("START LOOP");
+  test_claw();
 }
